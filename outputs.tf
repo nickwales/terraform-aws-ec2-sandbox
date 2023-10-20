eb4578475b9a0f3a3efb1eccd49a0d93a3549228
Nick Wales <588472+nickwales@users.noreply.github.com>
2023-10-20 10:20:14 -0500

    removes pre-modular files

--- outputs.tf
+++
@@ -1,7 +0,0 @@
-output "dc1_address" {
-  value = "http://${aws_instance.dc1.public_ip}:8500"
-}
-
-output "dc2_address" {
-  value = "http://${aws_instance.dc2.public_ip}:8500"
-}
--- variables.tf
+++
@@ -1,7 +0,0 @@
-variable "consul_token" {
-  default = "root"
-}
-
-variable "envoy_version" {
-  default = "1.24.1"
-}
