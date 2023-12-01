bbeb2252d8eac6b90bbf6a1f5bbaae97bf65cb69
Gerald Dykeman <gerald.dykeman@gmail.com>
2023-12-01 15:15:41 -0500

    removing dc2 as this is a single dc

--- projects/consul_single_dc_mesh/deploy.sh
+++ projects/consul_single_dc_mesh/deploy.sh
@@ -6,12 +6,6 @@ consul tls cert create -server
 mkdir certs_dc1
 mv *.pem certs_dc1
 
-consul tls ca create 
-consul tls cert create -dc dc2 -server
-mkdir certs_dc2
-mv *.pem certs_dc2
-
 consul keygen > certs_dc1/encryption_key
-consul keygen > certs_dc2/encryption_key
 
 terraform apply -auto-approve
