5691a5f6e0bc7ac608d99a856aae86485ce47644
Nick Wales <588472+nickwales@users.noreply.github.com>
2024-06-04 12:42:58 -0500

    Adds s3 bucket for snapshot backup

--- modules/consul_server/iam.tf
+++ modules/consul_server/iam.tf
@@ -23,23 +23,50 @@ resource "aws_iam_role" "consul_server" {
 }
 
 resource "aws_iam_role_policy" "consul_server" {
-    name_prefix = "${var.name}-${var.datacenter}"
+  name_prefix = "${var.name}-${var.datacenter}"
 
-    role = aws_iam_role.consul_server.id
-    policy = jsonencode({
-        Version = "2012-10-17"
-        Statement = [
-          {
-            Action = [
-                "ec2:DescribeInstances",
-                "ec2:DescribeTags",
-                "autoscaling:DescribeAutoScalingGroups",
-            ]
-            Effect = "Allow"
-            Resource = "*"
-          },
-        ]
-    })
+  role = aws_iam_role.consul_server.id
+  policy = jsonencode({
+      Version = "2012-10-17"
+      Statement = [
+        {
+          Action = [
+              "ec2:DescribeInstances",
+              "ec2:DescribeTags",
+              "autoscaling:DescribeAutoScalingGroups",
+          ]
+          Effect = "Allow"
+          Resource = "*"
+        },
+      ]
+  })
+}
+
+resource "aws_iam_role_policy" "consul_server_backup" {
+  name_prefix = "${var.name}-${var.datacenter}-s3-backup"
+
+  role = aws_iam_role.consul_server.id
+  policy = jsonencode({
+      Version = "2012-10-17"
+      Statement = [
+        {
+          "Effect": "Allow",
+          "Action": [
+              "s3:PutObject",
+              "s3:GetObject",
+              "s3:AbortMultipartUpload",
+              "s3:ListBucket",
+              "s3:DeleteObject",
+              "s3:GetObjectVersion",
+              "s3:ListMultipartUploadParts"
+          ],
+          "Resource": [
+              "arn:aws:s3:::${var.name}-consul-backup/*",
+              "arn:aws:s3:::${var.name}-consul-backup"
+          ]
+        }
+      ]
+  })
 }
 
 resource "aws_iam_role_policy_attachment" "read-only-attach" {
